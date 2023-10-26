7a281434eca4712ca70d057523ba47594d3152b5
carterp2112 <st0pandsm3lltheroses@gmail.com>
2023-10-26 12:48:34 -0500

    new commit

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ function getAvgPrice() {
     const price = document.getElementById("average-price")
     price.innerHTML = `The average price is currently $${Math.floor(avg)}`;
 }
+
+function displayFreelancers() {
+    const occupations = ["Janitor", "Day Laborer", "Teacher", "Writer", "Programmer"]
+    const names = ["John", "James", "Joe", "Dan", "Anne"]
+    let freelancers = []
+
+    for(let i = 0; i <= 5; i++) {
+        freelancers.push({cost: Math.floor(Math.random() * (100 - 10) ) + 10,
+                          job: occupations[Math.floor(Math.random() * 4)],
+                          name: names[Math.floor(Math.random() * 3)]});
+    }
+
+    function displayPerson(freelancers) {
+        const list = document.createElement("ul");
+        list.innerHTML = `<li>${freelancers[1].name}</li>\n<li>${freelancers[1].job}</li>\n<li>$${freelancers[1].cost}</li>`;
+        document.getElementById("listings").appendChild(list);
+    }
+    setInterval(displayPerson(freelancers), 1000);
+}
